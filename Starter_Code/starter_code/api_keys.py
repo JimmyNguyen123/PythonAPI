de79691a100845abdce8078ebc02db1f9a986038
JimmyNguyen123 <dnguy066@uottawa.ca>
2022-12-05 18:27:04 -0500

    5-Dec

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "268693f5d5eebef9916c6ba9a14b1941"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "4d1e2e74d9ba40c4b2a7409a9df994ee"
